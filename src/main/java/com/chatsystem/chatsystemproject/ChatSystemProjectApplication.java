d05091953c6e70a7c7ba177042f919b5e5c2eb1b
keisuke maekawa <vclkgmnrr6@outlook.jp>
2020-02-14 16:16:58 +0900

    初期構築

---
+++ src/main/java/com/chatsystem/chatsystemproject/ChatSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chatsystem.chatsystemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSystemProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatsystem/chatsystemproject/ChatSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatsystem.chatsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
