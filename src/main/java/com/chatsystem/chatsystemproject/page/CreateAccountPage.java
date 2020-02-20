16dbfc2d7e8bef7bb335ad0131ebde2076663f20
imoriyamori <cist.b218.k.aikawa@gmail.com>
2020-02-20 17:30:34 +0900

    アカウント作成フォーム(途中) 入力されたアカウント名とパスワードを標準出力できる

--- src/main/java/com/chatsystem/chatsystemproject/page/CreateAccountPage.java
+++ src/main/java/com/chatsystem/chatsystemproject/page/CreateAccountPage.java
@@ -2,5 +2,48 @@ package com.chatsystem.chatsystemproject.page;
 
 import org.apache.wicket.markup.html.WebPage;
 
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.model.Model;
+import org.wicketstuff.annotation.mount.MountPath;
+
+
+@MountPath("CreateAccount")
 public class CreateAccountPage extends WebPage {
+    public CreateAccountPage(){
+
+        var userNameModel = Model.of("");
+        var passwordModel = Model.of("");
+
+
+        var createAccountForm = new Form<>("CreateAccountForm");
+        add(createAccountForm);
+        createAccountForm.add(new Button("CreateAccountButton"){
+            @Override
+            public void onSubmit() {
+                var userName = userNameModel.getObject();
+                var password = passwordModel.getObject();
+                var msg = "送信データ："
+                        + userName
+                        + ","
+                        + password;
+                System.out.println(msg);
+            }
+        });
+
+        var userNameField = new TextField<>("UserNameField",userNameModel);
+        createAccountForm.add(userNameField);
+
+        var passwordField = new PasswordTextField("PasswordField",passwordModel);
+        createAccountForm.add(passwordField);
+
+        add(createAccountForm);
+
+    }
+
+
+
 }
