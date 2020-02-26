4c092caeb164e3bbb045144fc276d19a343f2f7d
snow-01 <cist.b218.t.mathumoto@gmail.com>
2020-02-26 15:04:33 +0900

    2/26 15:00 ThreadPage完成(削除ボタン以外)

--- src/main/java/com/chatsystem/chatsystemproject/page/ThreadPage.java
+++ src/main/java/com/chatsystem/chatsystemproject/page/ThreadPage.java
@@ -9,7 +9,6 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
@@ -17,8 +16,6 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
-
-import java.util.List;
 
 @AuthorizeInstantiation(Roles.USER)
 @MountPath("Thread")
@@ -45,10 +42,9 @@ public class ThreadPage extends WebPage {
                         //setResponsePage(new ManageThreadPage(senderUserNameModel));
                     }
                 };
-                listItem.add(new Label("SenderUserName",listItem.getModelObject().getSenderUserName()));
+                listItem.add(toManageUserPageLink.add(new Label("SenderUserName",listItem.getModelObject().getSenderUserName())));
                 listItem.add(new Label("Message",listItem.getModelObject().getMessage()));
                 listItem.add(new Label("PostTime",listItem.getModelObject().getPostTime()));
-                listItem.add(toManageUserPageLink);
             }
         };
 
